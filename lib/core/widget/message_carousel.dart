b52622cd9b474fe496198ef30369bffc01cd269e
LinoMacKay <51498397+LinoMacKay@users.noreply.github.com>
2023-01-23 11:48:35 -0500

    Update message_carousel.dart

--- lib/core/widget/message_carousel.dart
+++ lib/core/widget/message_carousel.dart
@@ -1,3 +1,4 @@
+import 'package:brokersdk/model/carousel_button.dart';
 import 'package:brokersdk/model/message_response.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -6,6 +7,23 @@ import 'package:flutter/src/widgets/framework.dart';
 class MessageCarousel extends StatelessWidget {
   List<MessageResponseData> data;
   MessageCarousel(this.data);
+
+  Widget getButton(List<CarouselButton> buttons) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: buttons.length,
+      itemBuilder: (context, indx) {
+        return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                buttons[indx].text!,
+              ),
+            ));
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +36,14 @@ class MessageCarousel extends StatelessWidget {
           controller: PageController(viewportFraction: 0.95),
           itemBuilder: (context, index) {
             return Container(
+              margin: const EdgeInsets.only(bottom: 20, right: 8),
               decoration: BoxDecoration(border: Border.all()),
               child: Column(
                 children: [
                   Image.network(data[index].mediaUrl!),
                   Text(data[index].title!),
                   Text(data[index].description!),
-                  ElevatedButton(onPressed: () {}, child: const Text("fasdfa"))
+                  getButton(data[index].buttons!)
                 ],
               ),
             );
